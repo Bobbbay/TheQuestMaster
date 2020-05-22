c24420089e4746356ab57e3198db96e37332e22c
Bobbbay Bobbayan <abatterysingle@gmail.com>
2020-05-22 14:12:36 -0400

    Security upgrades

--- src/bot.py
+++ src/bot.py
@@ -8,13 +8,13 @@ import praw
 import os
 import re
 
-reddit = praw.Reddit(client_id='T-VB-ecVtjieoA',
-                     client_secret='mC3X2iQkmS6EK6tQXKNg1mT6_H4',
-                     password='iamabot002',
-                     user_agent='r/redditsquestsbot',
+reddit = praw.Reddit(os.environ.get('client_id'),
+                     os.environ.get('client_secret)',
+                     os.environ.get('pass')
+                     user_agent='r/RedditsQuests bot',
                      username='TheQuestMaster')
 
-for submission in reddit.subreddit('bobbbaybots').new(limit=None):
+for submission in reddit.subreddit('RedditsQuests').new(limit=None):
     if 1:
         submission.comments.replace_more(limit=None)
         for comment in submission.comments.list():
@@ -23,18 +23,17 @@ for submission in reddit.subreddit('bobbbaybots').new(limit=None):
                     if comment.is_submitter:
                         if submission.saved is False:
                             completer = ""
-                            if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("Ջ"):
-                                count = int(comment.parent().author_flair_text.replace("Ջ",""))
+                            if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
+                                count = int(comment.parent().author_flair_text.replace("ᚬ",""))
                                 count += 1
-                                newflair = "{0}Ջ".format(count)
+                                newflair = "{0}ᚬ".format(count)
                                 completer = comment.parent().author.name
                                 reddit.subreddit('bobbbaybots').flair.set(comment.parent().author.name, newflair, "QuestFairer")
                                 print("EDITED FLAIR")
                             else:
-                                reddit.subreddit('bobbbaybots').flair.set(comment.parent().author.name, "1Ջ", "QuestFairer")
+                                reddit.subreddit('bobbbaybots').flair.set(comment.parent().author.name, "1ᚬ", "QuestFairer")
                                 print("NEWBYYYY")
                             submission.save()
                             reply = 'This quest has been completed by {0}, but feel free to go ahead and recomplete this quest! \n\n^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'.format(completer)
                             submission.reply(reply).mod.distinguish(sticky=True)
                     break
-    
