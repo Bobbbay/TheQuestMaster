eb1fa06ba3ee9ee802cae9d1672de518e5dc9849
Bobbbay Bobbayan <abatterysingle@gmail.com>
2020-05-22 14:55:23 -0400

    Edited bot reply text

--- src/bot.py
+++ src/bot.py
@@ -23,13 +23,19 @@ for submission in reddit.subreddit('RedditsQuests').new(limit=None):
                         if submission.saved is False:
                             completer = ""
                             if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
-                                count = int(comment.parent().author_flair_text.replace("ᚬ",""))
-                                count += 1
-                                newflair = "{0}ᚬ".format(count)
+                                count_op = int(comment.parent().author_flair_text.replace("ᚬ",""))
+                                count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
+                                count_op += 1
+                                count_taker += 2
+                                op_flair = "{0}ᚬ".format(count_op)
+                                taker_flair = "{0}ᚬ".format(count_taker)
                                 completer = comment.parent().author.name
-                                reddit.subreddit('RedditsQuests').flair.set(comment.parent().author.name, newflair, "QuestFairer")
+                                reddit.subreddit('RedditsQuests').flair.set(submission.author.name, op_flair, "QuestFairer")
+                                reddit.subreddit('RedditsQuests').flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
                             else:
-                                reddit.subreddit('RedditsQuests').flair.set(comment.parent().author.name, "1ᚬ", "QuestFairer")
+                                reddit.subreddit('RedditsQuests').flair.set(submission.author.name, "1ᚬ", "QuestFairer")
+                                reddit.subreddit('RedditsQuests').flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
+                            submission.flair.select(None, "Completed!")
                             submission.save()
                             reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! Upvote me if you think this quest was quite nice. Good quest creators and/or executors may recieve a prize!\n\n^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'.format(completer)
                             submission.reply(reply).mod.distinguish(sticky=True)
