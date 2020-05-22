457031cb824dfece985521bbf7408bf0ad196c61
Bobbay <49123594+Bobbbay@users.noreply.github.com>
2020-05-21 22:33:33 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,8 @@
+mport praw
+
+reddit = praw.Reddit(client_id="T-VB-ecVtjieoA",
+                             client_secret="mC3X2iQkmS6EK6tQXKNg1mT6_H4",
+                             user_agent="TheQuestMaster")
+
+for submission in reddit.subreddit("learnpython").hot(limit=10):
+        print(submission.title)
