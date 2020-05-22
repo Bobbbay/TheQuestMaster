8bfd422176e75791c6f4dfb9e149c786ee6119e1
Bobbbay Bobbayan <abatterysingle@gmail.com>
2020-05-22 14:56:54 -0400

    Added functionality and sub var

--- src/bot.py
+++ src/bot.py
@@ -8,13 +8,15 @@ import praw
 import os
 import re
 
+sub = "bobbbaybots"
+
 reddit = praw.Reddit(client_id=os.environ.get('client_id'),
                      client_secret=os.environ.get('client_secret'),
                      password=os.environ.get('pass'),
                      user_agent='r/RedditsQuests bot',
                      username='TheQuestMaster')
 
-for submission in reddit.subreddit('RedditsQuests').new(limit=None):
+for submission in reddit.subreddit(sub).new(limit=None):
     if 1:
         submission.comments.replace_more(limit=None)
         for comment in submission.comments.list():
@@ -30,11 +32,11 @@ for submission in reddit.subreddit('RedditsQuests').new(limit=None):
                                 op_flair = "{0}ᚬ".format(count_op)
                                 taker_flair = "{0}ᚬ".format(count_taker)
                                 completer = comment.parent().author.name
-                                reddit.subreddit('RedditsQuests').flair.set(submission.author.name, op_flair, "QuestFairer")
-                                reddit.subreddit('RedditsQuests').flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
+                                reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
+                                reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
                             else:
-                                reddit.subreddit('RedditsQuests').flair.set(submission.author.name, "1ᚬ", "QuestFairer")
-                                reddit.subreddit('RedditsQuests').flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
+                                reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
+                                reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
                             submission.flair.select(None, "Completed!")
                             submission.save()
                             reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! Upvote me if you think this quest was quite nice. Good quest creators and/or executors may recieve a prize!\n\n^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'.format(completer)
