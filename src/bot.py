5a89f6389e745ca657dd9e849e5a0523187e8b2b
Bobbbay <abatterysingle@gmail.com>
2020-05-24 17:39:52 -0400

    More and more fixes

--- src/bot.py
+++ src/bot.py
@@ -20,40 +20,37 @@ def find(string):
     return [x[0] for x in url] 
 
 for submission in reddit.subreddit(sub).new(limit=None):
-    if 1:
-        submission.comments.replace_more(limit=None)
-        for comment in submission.comments.list():
-            if (re.search('!completed', comment.body) is not None):
-                    if comment.is_submitter:
-                        if submission.saved is False:
-                            if (comment.parent().author.name is not submission.author.name):
-                                if(find(comment.parent().body) != []):
-                                    completer = ""
-                                    if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
-                                        count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
-                                        count_taker += 2
-                                        taker_flair = "{0}ᚬ".format(count_taker)
-                                        completer = comment.parent().author.name
-                                        reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
-                                    else:
-                                        reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
-
-                                    for flair in reddit.subreddit(sub).flair(redditor="Bobbbay", limit=None):
-                                        count_op_str = flair['flair_text']
-                                    if ( count_op_str != "" ):
-                                        count_op = int(count_op_str.replace("ᚬ", ""))
-                                        count_op += 1
-                                        op_flair = "{0}ᚬ".format(count_op)
-                                        reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
-                                    else:
-                                        reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
-
-                                    submission.flair.select(None, "Completed!")
-                                    submission.save()
-                                    reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\n^Upvote ^me ^if ^you ^think ^this ^quest ^was ^quite ^nice^. ^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'
-                                    submission.reply(reply).mod.distinguish(sticky=True)
+    submission.comments.replace_more(limit=None)
+    for comment in submission.comments.list():
+        if (re.search('!completed', comment.body) is not None):
+                if comment.is_submitter:
+                    if submission.saved is False:
+                        if (comment.parent().author.name is not submission.author.name):
+                            if(find(comment.parent().body) != []):
+                                completer = ""
+                                if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
+                                    count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
+                                    count_taker += 2
+                                    taker_flair = "{0}ᚬ".format(count_taker)
+                                    completer = comment.parent().author.name
+                                    reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
                                 else:
-                                    comment.mod.remove()
-                                    submission.author.message('Your comment in r/redditsquests has been removed', 'We only allow approvals that contain links. It\'s not your fault, but next time make sure the person who has completed the quest provides a link. PM u/MaxwellIsSmall for questions about this sub\'s rules and u/Bobbbay for any questions, concerns, or comments about the bot. ')
-                            break
-                            
+                                    reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
+                                for flair in reddit.subreddit(sub).flair(redditor="Bobbbay", limit=None):
+                                    count_op_str = flair['flair_text']
+                                if ( count_op_str != "" ):
+                                    count_op = int(count_op_str.replace("ᚬ", ""))
+                                    count_op += 1
+                                    op_flair = "{0}ᚬ".format(count_op)
+                                    reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
+                                else:
+                                    reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
+                                submission.flair.select(None, "Completed!")
+                                submission.save()
+                                reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\n^Upvote ^me ^if ^you ^think ^this ^quest ^was ^quite ^nice^. ^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'
+                                submission.reply(reply).mod.distinguish(sticky=True)
+                            else:
+                                comment.mod.remove()
+                                submission.author.message('Your comment in r/redditsquests has been removed', 'We only allow approvals that contain links. It\'s not your fault, but next time make sure the person who has completed the quest provides a link. PM u/MaxwellIsSmall for questions about this sub\'s rules and u/Bobbbay for any questions, concerns, or comments about the bot. ')
+                        break
+                        
