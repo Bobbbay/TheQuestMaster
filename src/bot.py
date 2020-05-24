2c1ba25558187aec8917f487d864d09b416b93de
Bobbbay <abatterysingle@gmail.com>
2020-05-24 17:35:32 -0400

    Added making sure there is a link

--- src/bot.py
+++ src/bot.py
@@ -14,6 +14,11 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent='r/RedditsQuests bot',
                      username='TheQuestMaster')
 
+def find(string): 
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex,string)       
+    return [x[0] for x in url] 
+
 for submission in reddit.subreddit(sub).new(limit=None):
     if 1:
         submission.comments.replace_more(limit=None)
@@ -22,33 +27,32 @@ for submission in reddit.subreddit(sub).new(limit=None):
                     if comment.is_submitter:
                         if submission.saved is False:
                             if (comment.parent().author.name is not submission.author.name):
-                                completer = ""
-                                if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
-                                    #count_op = int(submission.author.author_flair_text.replace("ᚬ",""))
-                                    count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
-                                    #count_op += 1
-                                    count_taker += 2
-                                    #op_flair = "{0}ᚬ".format(count_op)
-                                    taker_flair = "{0}ᚬ".format(count_taker)
-                                    completer = comment.parent().author.name
-                                    #reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
-                                    reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
-                                else:
-                                    #reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
-                                    reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
+                                if(find(comment.parent().body) != []):
+                                    completer = ""
+                                    if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
+                                        count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
+                                        count_taker += 2
+                                        taker_flair = "{0}ᚬ".format(count_taker)
+                                        completer = comment.parent().author.name
+                                        reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
+                                    else:
+                                        reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
 
-                                for flair in reddit.subreddit(sub).flair(redditor="Bobbbay", limit=None):
-                                    count_op_str = flair['flair_text']
-                                if ( count_op_str != "" ):
-                                    count_op = int(count_op_str.replace("ᚬ", ""))
-                                    count_op += 1
-                                    op_flair = "{0}ᚬ".format(count_op)
-                                    reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
-                                else:
-                                    reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
+                                    for flair in reddit.subreddit(sub).flair(redditor="Bobbbay", limit=None):
+                                        count_op_str = flair['flair_text']
+                                    if ( count_op_str != "" ):
+                                        count_op = int(count_op_str.replace("ᚬ", ""))
+                                        count_op += 1
+                                        op_flair = "{0}ᚬ".format(count_op)
+                                        reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
+                                    else:
+                                        reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
 
-                                submission.flair.select(None, "Completed!")
-                                submission.save()
-                                reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\n^Upvote ^me ^if ^you ^think ^this ^quest ^was ^quite ^nice^. ^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'
-                                submission.reply(reply).mod.distinguish(sticky=True)
-                        break
+                                    submission.flair.select(None, "Completed!")
+                                    submission.save()
+                                    reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\n^Upvote ^me ^if ^you ^think ^this ^quest ^was ^quite ^nice^. ^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'
+                                    submission.reply(reply).mod.distinguish(sticky=True)
+                            comment.mod.remove()
+                            submission.author.message('Your comment in r/redditsquests has been removed', 'We only allow approvals that contain links. It\'s not your fault, but next time make sure the person who has completed the quest provides a link. PM u/MaxwellIsSmall for questions about this sub\'s rules and u/Bobbbay for any questions, concerns, or comments about the bot. ')
+                            break
+                            
