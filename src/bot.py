13993c7146708640d94dae9828b2af4e448fa57a
Bobbbay <abatterysingle@gmail.com>
2020-05-24 14:00:52 -0400

    Fixed major point bug

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,7 @@ import praw
 import os
 import re
 
-sub = "RedditsQuests"
+sub = "BobbbayBots"
 
 client_id = os.environ.get('client_id')
 client_secret = os.environ.get('client_secret')
@@ -24,19 +24,30 @@ for submission in reddit.subreddit(sub).new(limit=None):
                             if (comment.parent().author.name is not submission.author.name):
                                 completer = ""
                                 if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
-                                    count_op = int(comment.parent().author_flair_text.replace("ᚬ",""))
+                                    #count_op = int(submission.author.author_flair_text.replace("ᚬ",""))
                                     count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
-                                    count_op += 1
+                                    #count_op += 1
                                     count_taker += 2
-                                    op_flair = "{0}ᚬ".format(count_op)
+                                    #op_flair = "{0}ᚬ".format(count_op)
                                     taker_flair = "{0}ᚬ".format(count_taker)
                                     completer = comment.parent().author.name
-                                    reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
+                                    #reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
                                     reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
                                 else:
-                                    reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
+                                    #reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
                                     reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
-                                submission.flair.select("4b693aec-88af-11ea-987a-0ed65c655b8f", "Completed!")
+
+                                for flair in reddit.subreddit(sub).flair(redditor="Bobbbay", limit=None):
+                                    count_op_str = flair['flair_text']
+                                if ( count_op_str != "" ):
+                                    count_op = int(count_op_str.replace("ᚬ", ""))
+                                    count_op += 1
+                                    op_flair = "{0}ᚬ".format(count_op)
+                                    reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
+                                else:
+                                    reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
+
+                                #submission.flair.select("4b693aec-88af-11ea-987a-0ed65c655b8f", "Completed!")
                                 submission.save()
                                 reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\n^Upvote ^me ^if ^you ^think ^this ^quest ^was ^quite ^nice^. ^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'
                                 submission.reply(reply).mod.distinguish(sticky=True)
