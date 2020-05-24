9458cec4aa2716e1c35e82b14897cbe2ce4fd06e
Bobbbay <abatterysingle@gmail.com>
2020-05-24 11:37:26 -0400

    Added feature to disable self points

--- src/bot.py
+++ src/bot.py
@@ -21,22 +21,23 @@ for submission in reddit.subreddit(sub).new(limit=None):
             if (re.search('!completed', comment.body) is not None):
                     if comment.is_submitter:
                         if submission.saved is False:
-                            completer = ""
-                            if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
-                                count_op = int(comment.parent().author_flair_text.replace("ᚬ",""))
-                                count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
-                                count_op += 1
-                                count_taker += 2
-                                op_flair = "{0}ᚬ".format(count_op)
-                                taker_flair = "{0}ᚬ".format(count_taker)
-                                completer = comment.parent().author.name
-                                reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
-                                reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
-                            else:
-                                reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
-                                reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
-                            submission.flair.select("4b693aec-88af-11ea-987a-0ed65c655b8f", "Completed!")
-                            submission.save()
-                            reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\nUpvote me if you think this quest was quite nice. Good quest creators and/or executors may recieve a prize!\n\n^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'.format(completer)
-                            submission.reply(reply).mod.distinguish(sticky=True)
-                    break
+                            if (comment.parent().author.name is not submission.author.name):
+                                completer = ""
+                                if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
+                                    count_op = int(comment.parent().author_flair_text.replace("ᚬ",""))
+                                    count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
+                                    count_op += 1
+                                    count_taker += 2
+                                    op_flair = "{0}ᚬ".format(count_op)
+                                    taker_flair = "{0}ᚬ".format(count_taker)
+                                    completer = comment.parent().author.name
+                                    reddit.subreddit(sub).flair.set(submission.author.name, op_flair, "QuestFairer")
+                                    reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
+                                else:
+                                    reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
+                                    reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
+                                submission.flair.select("4b693aec-88af-11ea-987a-0ed65c655b8f", "Completed!")
+                                submission.save()
+                                reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\nUpvote me if you think this quest was quite nice. Good quest creators and/or executors may recieve a prize!\n\n^Beep ^boop^. ^Contact ^my ^creator ^Bobbbay^.'
+                                submission.reply(reply).mod.distinguish(sticky=True)
+                        break
