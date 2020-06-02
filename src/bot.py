409b6eb22626f4505bde8dbbb1b8b8fe8a45e32d
Bobbbay <abatterysingle@gmail.com>
2020-06-02 19:08:37 -0400

    Hey

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,6 @@
 import praw
 import os
 import re
-#from bs4 import BeautifulSoup as BS
 
 sub = "RedditsQuests"
 
@@ -20,14 +19,20 @@ def find(string):
     url = re.findall(regex,string)       
     return [x[0] for x in url] 
 
-for submission in reddit.subreddit(sub).new(limit=1):
+for submission in reddit.subreddit(sub).new(limit=None):
+    print("Started")
     submission.comments.replace_more(limit=None)
+    print("Replaced more?")
     if submission.saved is False:
+        print("Submission is not saved yet xD")
         for comment in submission.comments.list():
+            print("Going through comments")
             if ((re.search('!completed', comment.body, re.IGNORECASE) is not None) and (comment.is_submitter or 'RedditsQuests' in comment.author.moderated()) and (comment.parent().author.name is not submission.author.name)):
                 #if(find(comment.parent().body) != []):
+                print("Wow big if statement")
                 count_op_str = submission.author_flair_text
                 try:
+                    print("Trying this thing I should not be doing xD")
                     if ( count_op_str is not None or count_op_str != ""):
                         count_op = int(count_op_str.replace("ᚬ", ""))
                         count_op += 1
@@ -36,18 +41,22 @@ for submission in reddit.subreddit(sub).new(limit=1):
                     else:
                         reddit.subreddit(sub).flair.set(submission.author.name, "1ᚬ", "QuestFairer")
                 except:
-                    print()
+                    print("You have been accepted")
                 if comment.parent().author_flair_text and comment.parent().author_flair_text.endswith("ᚬ"):
+                    print("Adding to the old quest doer")
                     count_taker = int(comment.parent().author_flair_text.replace("ᚬ",""))
                     count_taker += 2
                     taker_flair = "{0}ᚬ".format(count_taker)
                     reddit.subreddit(sub).flair.set(comment.parent().author.name, taker_flair, "QuestFairer")
                 else:
+                    print("Adding to the new quest doer")
                     reddit.subreddit(sub).flair.set(comment.parent().author.name, "2ᚬ", "QuestFairer")
+                print("All done, adding flairs now")
                 submission.flair.select(None, "Completed!")
                 submission.save()
                 reply = 'This quest has been completed, but feel free to go ahead and recomplete this quest! \n\n^Beep ^boop^.'
                 submission.reply(reply).mod.distinguish(sticky=True)
+                print("Finished! Breaking...")
                 #else:
                     #comment.mod.remove()
                     #submission.author.message('Your comment in r/redditsquests has been removed', 'We only allow approvals that contain links. It\'s not your fault, but next time make sure the person who has completed the quest provides a link. PM u/MaxwellIsSmall for questions about this sub\'s rules and u/Bobbbay for any questions, concerns, or comments about the bot. ')
