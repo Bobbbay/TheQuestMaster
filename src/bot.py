d40b667d48b53c1236eee3e196325bede1856de5
Bobbbay <abatterysingle@gmail.com>
2020-06-02 22:44:25 -0400

    Test

--- src/bot.py
+++ src/bot.py
@@ -61,4 +61,4 @@ for submission in reddit.subreddit(sub).new(limit=None):
                 #else:
                     #comment.mod.remove()
                     #submission.author.message('Your comment in r/redditsquests has been removed', 'We only allow approvals that contain links. It\'s not your fault, but next time make sure the person who has completed the quest provides a link. PM u/MaxwellIsSmall for questions about this sub\'s rules and u/Bobbbay for any questions, concerns, or comments about the bot. ')
-break
+    break
